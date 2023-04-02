e294d27d718ad8171991272fd8430eb6bf6c6360
ak5k <42914711+ak5k@users.noreply.github.com>
2023-04-03 02:03:25 +0300

    dev

---
+++ reaper-plugins/reaper_csurf/csurf.h
@@ -0,0 +1,101 @@
+#ifndef _CSURF_H_
+#define _CSURF_H_
+
+#include "../localize.h"
+
+#include "../../WDL/db2val.h"
+#include "../../WDL/wdlcstring.h"
+#include "../../WDL/wdlstring.h"
+#include "../../WDL/win32_utf8.h"
+#include "resource.h"
+#include <stdio.h>
+
+enum {
+    HZOOM_EDITPLAYCUR = 0,
+    HZOOM_EDITCUR = 1,
+    HZOOM_VIEWCTR = 2,
+    HZOOM_MOUSECUR = 3
+};
+
+enum {
+    VZOOM_VIEWCTR = 0,
+    VZOOM_TOPVIS = 1,
+    VZOOM_LASTSEL = 2,
+    VZOOM_MOUSECUR = 3
+};
+
+extern REAPER_PLUGIN_HINSTANCE g_hInst; // used for dialogs
+extern HWND g_hwnd;
+
+extern int* g_config_csurf_rate;
+extern int* g_config_zoommode;
+
+extern int* g_vu_minvol;
+extern int* g_vu_maxvol;
+extern int* g_config_vudecay;
+
+extern int __g_projectconfig_timemode2;
+extern int __g_projectconfig_timemode;
+extern int __g_projectconfig_timeoffs;
+extern int __g_projectconfig_measoffs;
+extern int __g_projectconfig_autoxfade;
+extern int __g_projectconfig_metronome_en;
+
+/*
+** REAPER command message defines
+*/
+
+#define IDC_REPEAT 1068
+#define ID_FILE_SAVEAS 40022
+#define ID_FILE_NEWPROJECT 40023
+#define ID_FILE_OPENPROJECT 40025
+#define ID_FILE_SAVEPROJECT 40026
+#define IDC_EDIT_UNDO 40029
+#define IDC_EDIT_REDO 40030
+#define ID_MARKER_PREV 40172
+#define ID_MARKER_NEXT 40173
+#define ID_INSERT_MARKERRGN 40174
+#define ID_INSERT_MARKER 40157
+#define ID_LOOP_SETSTART 40222
+#define ID_LOOP_SETEND 40223
+#define ID_METRONOME 40364
+#define ID_GOTO_MARKER1 40161
+#define ID_SET_MARKER1 40657
+
+// Reaper track automation modes
+enum AutoMode {
+    AUTO_MODE_TRIM,
+    AUTO_MODE_READ,
+    AUTO_MODE_TOUCH,
+    AUTO_MODE_WRITE,
+    AUTO_MODE_LATCH,
+};
+
+#define DELETE_ASYNC(x)     \
+    do {                    \
+        if (x)              \
+            (x)->Destroy(); \
+    } while (0)
+
+midi_Output* CreateThreadedMIDIOutput(
+    midi_Output* output); // returns null on null
+
+#define PREF_DIRCH WDL_DIRCHAR
+#define PREF_DIRSTR WDL_DIRCHAR_STR
+
+#define DEFAULT_DEVICE_REMAP()                       \
+    if (call == CSURF_EXT_MIDI_DEVICE_REMAP) {       \
+        if ((int)(INT_PTR)parm1 == 0 &&              \
+            m_midi_in_dev == (int)(INT_PTR)parm2) {  \
+            m_midi_in_dev = (int)(INT_PTR)parm3;     \
+            return 1;                                \
+        }                                            \
+        else if (                                    \
+            (int)(INT_PTR)parm1 == 1 &&              \
+            m_midi_out_dev == (int)(INT_PTR)parm2) { \
+            m_midi_out_dev = (int)(INT_PTR)parm3;    \
+            return 1;                                \
+        }                                            \
+    }
+
+#endif
---
+++ reaper-plugins/reaper_csurf/csurf_main.cpp
@@ -0,0 +1,241 @@
+/*
+** reaper_csurf
+** Copyright (C) 2006-2008 Cockos Incorporated
+** License: LGPL.
+*/
+
+#define REAPERAPI_IMPLEMENT
+#include <reaper_plugin_functions.h>
+
+#define LOCALIZE_IMPORT_PREFIX "csurf_"
+
+#include "../localize-import.h"
+#include "csurf.h"
+
+#include "../../WDL/win32_utf8.c"
+
+#include "../../WDL/setthreadname.h"
+
+extern reaper_csurf_reg_t csurf_mcu_reg, csurf_mcuex_reg;
+
+REAPER_PLUGIN_HINSTANCE g_hInst; // used for dialogs, if any
+HWND g_hwnd;
+
+int* g_config_csurf_rate;
+int* g_config_zoommode;
+
+int* g_vu_minvol;
+int* g_vu_maxvol;
+int* g_config_vudecay;
+
+int __g_projectconfig_timemode2;
+int __g_projectconfig_timemode;
+int __g_projectconfig_measoffs;
+int __g_projectconfig_timeoffs; // double
+int __g_projectconfig_show_grid;
+int __g_projectconfig_autoxfade;
+int __g_projectconfig_metronome_en;
+
+extern "C" {
+
+REAPER_PLUGIN_DLL_EXPORT int REAPER_PLUGIN_ENTRYPOINT(
+    REAPER_PLUGIN_HINSTANCE hInstance,
+    reaper_plugin_info_t* rec)
+{
+    g_hInst = hInstance;
+
+    if (!rec || rec->caller_version != REAPER_PLUGIN_VERSION || !rec->GetFunc ||
+        REAPERAPI_LoadAPI(rec->GetFunc))
+        return 0;
+
+    g_hwnd = rec->hwnd_main;
+    int errcnt = 0;
+
+    int sztmp;
+#define IMPVAR(x, nm)                                      \
+    if (!((*(void**)&(x)) = get_config_var(nm, &sztmp)) || \
+        sztmp != sizeof(*x))                               \
+        errcnt++;
+#define IMPVARP(x, nm, type)                              \
+    if (!((x) = projectconfig_var_getoffs(nm, &sztmp)) || \
+        sztmp != sizeof(type))                            \
+        errcnt++;
+    IMPVAR(g_config_csurf_rate, "csurfrate")
+    IMPVAR(g_config_zoommode, "zoommode")
+    IMPVAR(g_vu_minvol, "vuminvol");
+    IMPVAR(g_vu_maxvol, "vumaxvol");
+    IMPVAR(g_config_vudecay, "vudecay");
+
+    IMPVARP(__g_projectconfig_timemode, "projtimemode", int)
+    IMPVARP(__g_projectconfig_timemode2, "projtimemode2", int)
+    IMPVARP(__g_projectconfig_timeoffs, "projtimeoffs", double);
+    IMPVARP(__g_projectconfig_measoffs, "projmeasoffs", int);
+    IMPVARP(__g_projectconfig_show_grid, "projshowgrid", int);
+    IMPVARP(__g_projectconfig_autoxfade, "autoxfade", int);
+    IMPVARP(__g_projectconfig_metronome_en, "projmetroen", int);
+
+    if (errcnt)
+        return 0;
+
+    // Plugin_Register = rec->Register;
+
+    rec->Register("csurf", &csurf_mcu_reg);
+    rec->Register("csurf", &csurf_mcuex_reg);
+
+    IMPORT_LOCALIZE_RPLUG(rec)
+    return 1;
+}
+};
+
+#ifndef _WIN32 // MAC resources
+#include "../../WDL/swell/swell-dlggen.h"
+#include "res.rc_mac_dlg"
+#undef BEGIN
+#undef END
+#include "../../WDL/swell/swell-menugen.h"
+#include "res.rc_mac_menu"
+#endif
+
+#ifndef _WIN32 // let OS X use this threading step
+
+#include "../../WDL/mutex.h"
+#include "../../WDL/ptrlist.h"
+
+class threadedMIDIOutput : public midi_Output {
+  public:
+    threadedMIDIOutput(midi_Output* out)
+    {
+        m_output = out;
+        m_quit = 0;
+        unsigned id;
+        m_hThread = (HANDLE)_beginthreadex(NULL, 0, threadProc, this, 0, &id);
+    }
+
+    virtual ~threadedMIDIOutput()
+    {
+        if (m_hThread) {
+            m_quit = 1;
+            WaitForSingleObject(m_hThread, INFINITE);
+            CloseHandle(m_hThread);
+            m_hThread = 0;
+            Sleep(30);
+        }
+
+        if (m_output)
+            m_output->Destroy();
+        m_empty.Empty(true);
+        m_full.Empty(true);
+    }
+
+    virtual void Destroy()
+    {
+        HANDLE thread = m_hThread;
+        if (!thread) {
+            delete this;
+        }
+        else {
+            m_hThread = NULL;
+            m_quit = 2;
+
+            // thread will delete self
+            WaitForSingleObject(thread, 100);
+            CloseHandle(thread);
+        }
+    }
+
+    virtual void SendMsg(
+        MIDI_event_t* msg,
+        int frame_offset) // frame_offset can be <0 for "instant" if supported
+    {
+        if (!msg)
+            return;
+
+        WDL_HeapBuf* b = NULL;
+        if (m_empty.GetSize()) {
+            m_mutex.Enter();
+            b = m_empty.Get(m_empty.GetSize() - 1);
+            m_empty.Delete(m_empty.GetSize() - 1);
+            m_mutex.Leave();
+        }
+        if (!b && m_empty.GetSize() + m_full.GetSize() < 500)
+            b = new WDL_HeapBuf(256);
+
+        if (b) {
+            int sz = msg->size;
+            if (sz < 3)
+                sz = 3;
+            int len = msg->midi_message + sz - (unsigned char*)msg;
+            memcpy(b->Resize(len, false), msg, len);
+            m_mutex.Enter();
+            m_full.Add(b);
+            m_mutex.Leave();
+        }
+    }
+
+    virtual void Send(
+        unsigned char status,
+        unsigned char d1,
+        unsigned char d2,
+        int frame_offset) // frame_offset can be <0 for "instant" if supported
+    {
+        MIDI_event_t evt = {0, 3, status, d1, d2};
+        SendMsg(&evt, frame_offset);
+    }
+
+    ///////////
+
+    static unsigned WINAPI threadProc(LPVOID p)
+    {
+        WDL_SetThreadName("reaper/cs_midio");
+        WDL_HeapBuf* lastbuf = NULL;
+        threadedMIDIOutput* _this = (threadedMIDIOutput*)p;
+        unsigned int scnt = 0;
+        for (;;) {
+            if (_this->m_full.GetSize() || lastbuf) {
+                _this->m_mutex.Enter();
+                if (lastbuf)
+                    _this->m_empty.Add(lastbuf);
+                lastbuf = _this->m_full.Get(0);
+                _this->m_full.Delete(0);
+                _this->m_mutex.Leave();
+
+                if (lastbuf)
+                    _this->m_output->SendMsg((MIDI_event_t*)lastbuf->Get(), -1);
+                scnt = 0;
+            }
+            else {
+                Sleep(1);
+                if (_this->m_quit && scnt++ > 3)
+                    break; // only quit once all messages have been sent
+            }
+        }
+        delete lastbuf;
+        if (_this->m_quit == 2)
+            delete _this;
+        return 0;
+    }
+
+    WDL_Mutex m_mutex;
+    WDL_PtrList<WDL_HeapBuf> m_full, m_empty;
+
+    HANDLE m_hThread;
+    int m_quit; // set to 1 to finish, 2 to finish+delete self
+    midi_Output* m_output;
+};
+
+midi_Output* CreateThreadedMIDIOutput(midi_Output* output)
+{
+    if (!output)
+        return output;
+    return new threadedMIDIOutput(output);
+}
+
+#else
+
+// windows doesnt need it since we have threaded midi outputs now
+midi_Output* CreateThreadedMIDIOutput(midi_Output* output)
+{
+    return output;
+}
+
+#endif
